cca8894060725d2fbf9063f314443d3b61278e1a
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-10-12 00:01:57 +0900

    호텔 방 배정하기(Level 4) 코드 추가

---
+++ src/graph/호텔_방_배정하기.java
@@ -0,0 +1,25 @@
+package graph;
+
+import java.util.*;
+public class 호텔_방_배정하기 {
+
+    public long[] solution(long k, long[] room_number) {
+        Map<Long, Long> reserved = new LinkedHashMap<>();
+
+        for (long room : room_number) {
+            List<Long> visited = new ArrayList<>();
+            visited.add(room);
+
+            while (reserved.containsKey(room)) {
+                room = reserved.get(room);
+                visited.add(room);
+            }
+
+            for (long prev : visited) {
+                reserved.put(prev, room + 1);
+            }
+        }
+
+        return reserved.keySet().stream().mapToLong(Long::longValue).toArray();
+    }
+}

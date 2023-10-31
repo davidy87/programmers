2aafd92767c2cfa2f6082c8cb8f3f08ef0f12fd8
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-10-31 17:11:32 +0900

    체육대회(Level 2) 코드 추가

---
+++ src/PCCP_모의고사/체육대회.java
@@ -0,0 +1,27 @@
+package PCCP_모의고사;
+
+public class 체육대회 {
+
+    private static int maxScore = 0;
+
+    public int solution(int[][] ability) {
+        dfs(ability, new boolean[ability.length], 0, 0);
+
+        return maxScore;
+    }
+
+    private void dfs(int[][] ability, boolean[] selected, int idx, int score) {
+        if (idx == ability[0].length) {
+            maxScore = Math.max(score, maxScore);
+            return;
+        }
+
+        for (int i = 0; i < ability.length; i++) {
+            if (!selected[i]) {
+                selected[i] = true;
+                dfs(ability, selected, idx + 1, score + ability[i][idx]);
+                selected[i] = false;
+            }
+        }
+    }
+}

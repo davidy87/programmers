7ff1ba98a40940b903db5dff3dea14781372c967
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-10-31 17:10:27 +0900

    외톨이 알파벳(Level 1) 코드 추가

---
+++ src/PCCP_모의고사/외톨이_알파벳.java
@@ -0,0 +1,30 @@
+package PCCP_모의고사;
+
+import java.util.*;
+
+public class 외톨이_알파벳 {
+
+    public String solution(String input_string) {
+        StringBuilder sb = new StringBuilder();
+        Set<Character> appeared = new HashSet<>();
+        Set<Character> answerSet = new TreeSet<>();
+        char prev = ' ';
+
+        for (char c : input_string.toCharArray()) {
+            if (c != prev) {
+                if (appeared.contains(c)) {
+                    answerSet.add(c);
+                }
+
+                appeared.add(c);
+                prev = c;
+            }
+        }
+
+        for (char c : answerSet) {
+            sb.append(c);
+        }
+
+        return sb.length() == 0 ? "N" : sb.toString();
+    }
+}

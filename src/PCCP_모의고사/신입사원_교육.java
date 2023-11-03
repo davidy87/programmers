ae2b45d0eece44e0c709409c172cbd4046395efc
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-11-03 17:49:24 +0900

    신입사원 교육(Level 2) 코드 추가

---
+++ src/PCCP_모의고사/신입사원_교육.java
@@ -0,0 +1,29 @@
+package PCCP_모의고사;
+
+import java.util.*;
+
+public class 신입사원_교육 {
+
+    public int solution(int[] ability, int number) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int a : ability) {
+            pq.offer(a);
+        }
+
+        for (int i = 0; i < number; i++) {
+            int sum = pq.poll() + pq.poll();
+
+            for (int j = 0; j < 2; j++) {
+                pq.offer(sum);
+            }
+        }
+
+        while (!pq.isEmpty()) {
+            answer += pq.poll();
+        }
+
+        return answer;
+    }
+}

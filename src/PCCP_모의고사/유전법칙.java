da7c111f9535815dd775ae5defe6a5dac38aab07
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2023-11-03 17:26:37 +0900

    유전법칙(Level 2) 코드 수정

--- src/PCCP_모의고사/유전법칙.java
+++ src/PCCP_모의고사/유전법칙.java
@@ -6,25 +6,28 @@ public class 유전법칙 {
         String[] answer = new String[queries.length];
 
         for (int i = 0; i < queries.length; i++) {
-            int n = queries[i][0];
-            int p = queries[i][1];
-            answer[i] = n == 1 ? "Rr" : search(n, p);
+            answer[i] = search(queries[i][0], queries[i][1]);
         }
 
         return answer;
     }
 
     private String search(int n, int p) {
-        int numPerGen = (int) Math.pow(4, n - 1);
+        if (n == 1) {
+            return "Rr";
+        }
 
-        if (p <= numPerGen / 4) {
+        int numPerGen = (int) Math.pow(4, n - 1);
+        int numPerPea = numPerGen / 4;
+
+        if (p <= numPerPea) {
             return "RR";
-        } else if (p > numPerGen / 4 * 3) {
+        } else if (p > numPerPea * 3) {
             return "rr";
         } else if (n == 2) {
             return "Rr";
-        } else if (p > numPerGen / 4 && p <= numPerGen / 2) {
-            return search(n - 1, p - numPerGen / 4);
+        } else if (p <= numPerGen / 2) {
+            return search(n - 1, p - numPerPea);
         }
 
         return search(n - 1, p - numPerGen / 2);

d825b01ccd931f4322ea8260c18606dc4912e194
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2024-01-14 11:54:56 +0900

    문제 풀이 - 네트워크 (Lv. 3)

---
+++ src/lv3/네트워크.java
@@ -0,0 +1,28 @@
+package lv3;
+
+public class 네트워크 {
+
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] visited = new boolean[n];
+
+        for (int c1 = 0; c1 < n; c1++) {
+            if (!visited[c1]) {
+                search(n, c1, computers, visited);
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    private void search(int n, int c1, int[][] computers, boolean[] visited) {
+        visited[c1] = true;
+
+        for (int c2 = 0; c2 < n; c2++) {
+            if (computers[c1][c2] == 1 && !visited[c2]) {
+                search(n, c2, computers, visited);
+            }
+        }
+    }
+}

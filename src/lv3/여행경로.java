0b1a034be148e6b2411e8adec7b13094aa661ba7
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2024-01-28 15:03:10 +0900

    문제 풀이 - 여행경로 (Lv. 3)

---
+++ src/lv3/여행경로.java
@@ -0,0 +1,35 @@
+package lv3;
+
+import java.util.*;
+
+public class 여행경로 {
+
+    private boolean[] visited;
+    private Queue<String> routes;
+
+    public String[] solution(String[][] tickets) {
+        String[] answer = {};
+        visited = new boolean[tickets.length];
+        routes = new PriorityQueue<>();
+        findRoutes(tickets, "ICN", new StringBuilder("ICN"), 0);
+
+        return routes.peek().split(" ");
+    }
+
+    private void findRoutes(String[][] tickets, String cur, StringBuilder route, int count) {
+        if (count == tickets.length) {
+            routes.offer(route.toString());
+            return;
+        }
+
+        for (int i = 0; i < tickets.length; i++) {
+            if (!visited[i] && tickets[i][0].equals(cur)) {
+                visited[i] = true;
+                route.append(" ").append(tickets[i][1]);
+                findRoutes(tickets, tickets[i][1], route, count + 1);
+                route.delete(route.length() - 4, route.length());
+                visited[i] = false;
+            }
+        }
+    }
+}

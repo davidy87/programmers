ee16fa863001275f56cce6449b2230dc2204fc27
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2024-02-10 11:17:29 +0900

    문제 풀이 - 연속 펄스 부분 수열의 합 (Lv. 3)

---
+++ src/dp/연속_펄스_부분_수열의_합.java
@@ -0,0 +1,22 @@
+package dp;
+
+public class 연속_펄스_부분_수열의_합 {
+
+    public long solution(int[] sequence) {
+        long answer = 0;
+        long[][] dp = new long[sequence.length][2];
+
+        dp[0][0] = sequence[0];
+        dp[0][1] = -sequence[0];
+        answer = Math.max(dp[0][0], dp[0][1]);
+
+        for (int i = 1; i < sequence.length; i++) {
+            long s = sequence[i];
+            dp[i][0] = Math.max(s, dp[i - 1][1] + s);
+            dp[i][1] = Math.max(-s, dp[i - 1][0] - s);
+            answer = Math.max(answer, Math.max(dp[i][0], dp[i][1]));
+        }
+
+        return answer;
+    }
+}

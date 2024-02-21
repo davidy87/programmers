eb7351dd139594f717915340168a6ad499d1dff5
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2024-02-21 14:29:48 +0900

    문제 풀이 - 거스름돈 (Lv. 3)

---
+++ src/dp/거스름돈.java
@@ -0,0 +1,22 @@
+package dp;
+
+import java.util.Arrays;
+
+public class 거스름돈 {
+
+    private static final int DIV = 1_000_000_007;
+
+    public int solution(int n, int[] money) {
+        Arrays.sort(money);
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        for (int m : money) {
+            for (int i = m; i <= n; i++) {
+                dp[i] = (dp[i] + dp[i - m]) % DIV;
+            }
+        }
+
+        return dp[n];
+    }
+}

0d70203b357c81bd3fcf8e2378de39a2c2b3ae18
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2024-03-01 12:49:34 +0900

    문제 풀이 - 표 편집 (Lv. 3)

---
+++ src/kakao_intern_2021/표_편집.java
@@ -0,0 +1,39 @@
+package kakao_intern_2021;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class 표_편집 {
+
+    public String solution(int n, int k, String[] cmd) {
+        Deque<Integer> deleted = new ArrayDeque<>();
+
+        for (String c : cmd) {
+            String[] split = c.split(" ");
+
+            switch (split[0]) {
+                case "U" -> k -= Integer.parseInt(split[1]);
+                case "D" -> k += Integer.parseInt(split[1]);
+                case "C" -> {
+                    deleted.offerLast(k);
+                    if (k == n - deleted.size()) {
+                        k--;
+                    }
+                }
+                case "Z" -> {
+                    if (deleted.pollLast() <= k) {
+                        k++;
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder("O".repeat(n - deleted.size()));
+
+        while (!deleted.isEmpty()) {
+            sb.insert(deleted.pollLast(), "X");
+        }
+
+        return sb.toString();
+    }
+}

87bc6447f69ff2d29fb71bf3db7b017139fc7df0
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2024-04-17 11:55:41 +0900

    문제 풀이 - 캐시(Lv. 2), 의상(Lv. 2)

---
+++ src/algo/hash/의상.java
@@ -0,0 +1,22 @@
+package algo.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 의상 {
+
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> counter = new HashMap<>();
+
+        for (String[] cloth : clothes) {
+            counter.put(cloth[1], counter.getOrDefault(cloth[1], 0) + 1);
+        }
+
+        for (int count : counter.values()) {
+            answer *= count + 1;
+        }
+
+        return answer - 1;
+    }
+}
---
+++ src/kakao/blind/_2018/캐시.java
@@ -0,0 +1,38 @@
+package kakao.blind._2018;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class 캐시 {
+    private static final int HIT = 1;
+    private static final int MISS = 5;
+
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        Deque<String> cache = new ArrayDeque<>();
+
+        if (cacheSize == 0) {
+            return cities.length * MISS;
+        }
+
+        for (String city : cities) {
+            city = city.toLowerCase();
+
+            if (cache.contains(city)) {
+                cache.remove(city);
+                cache.offer(city);
+                answer += HIT;
+                continue;
+            }
+
+            if (cache.size() >= cacheSize) {
+                cache.poll();
+            }
+
+            cache.offer(city);
+            answer += MISS;
+        }
+
+        return answer;
+    }
+}

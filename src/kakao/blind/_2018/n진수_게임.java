a22a6fb334c27901d6e879ef242a63d0cf89cdc2
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2024-04-28 10:49:34 +0900

    문제 풀이 - 압축(Lv. 2) & n진수 게임(Lv. 2)

---
+++ src/kakao/blind/_2018/n진수_게임.java
@@ -0,0 +1,26 @@
+package kakao.blind._2018;
+
+public class n진수_게임 {
+
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder sb = new StringBuilder(t);
+        int turn = 0;
+        int curNum = 0;
+
+        while (sb.length() < t) {
+            String numStr = Integer.toString(curNum++, n);
+
+            for (char c : numStr.toCharArray()) {
+                if (turn++ % m == p - 1) {
+                    sb.append(Character.toUpperCase(c));
+                }
+
+                if (sb.length() == t) {
+                    break;
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
---
+++ src/kakao/blind/_2018/압축.java
@@ -0,0 +1,51 @@
+package kakao.blind._2018;
+
+import java.util.*;
+
+public class 압축 {
+
+    public int[] solution(String msg) {
+        List<Integer> indices = new ArrayList<>();
+        Map<String, Integer> dict = initDict();
+        Queue<Character> queue = getCharQueue(msg);
+        int nextIdx = 'Z' - 'A' + 2;
+
+        while (!queue.isEmpty()) {
+            StringBuilder w = new StringBuilder();
+
+            while (!queue.isEmpty() && dict.containsKey(w.toString() + queue.peek())) {
+                w.append(queue.poll());
+            }
+
+            indices.add(dict.get(w.toString()));
+
+            if (!queue.isEmpty()) {
+                dict.put(w.toString() + queue.peek(), nextIdx++);
+            }
+        }
+
+        return indices.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private Queue<Character> getCharQueue(String msg) {
+        Queue<Character> queue = new LinkedList<>();
+
+        for (char c : msg.toCharArray()) {
+            queue.offer(c);
+        }
+
+        return queue;
+    }
+
+    private Map<String, Integer> initDict() {
+        Map<String, Integer> dict = new HashMap<>();
+        char c = 'A';
+
+        while (c <= 'Z') {
+            dict.put(Character.toString(c), c - 'A' + 1);
+            c += 1;
+        }
+
+        return dict;
+    }
+}

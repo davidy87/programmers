cecdf1f368be4173b76d124eff281ed4072f4f1c
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2024-04-30 10:23:43 +0900

    문제 풀이 - 더 맵게(Lv. 2) & 방문 길이(Lv. 2)

---
+++ src/lv2/더_맵게.java
@@ -0,0 +1,24 @@
+package lv2;
+
+import java.util.PriorityQueue;
+
+public class 더_맵게 {
+
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int s : scoville) {
+            pq.offer(s);
+        }
+
+        while (pq.peek() < K && pq.size() >= 2) {
+            int s1 = pq.poll();
+            int s2 = pq.poll();
+            pq.offer(s1 + s2 * 2);
+            answer++;
+        }
+
+        return pq.peek() >= K ? answer : -1;
+    }
+}
---
+++ src/summer_winter_coding/방문_길이.java
@@ -0,0 +1,82 @@
+package summer_winter_coding;
+
+import java.util.*;
+
+public class 방문_길이 {
+
+    private static final Map<String, int[]> moves = Map.of(
+            "U", new int[] {0, 1},
+            "D", new int[] {0, -1},
+            "R", new int[] {1, 0},
+            "L", new int[] {-1, 0}
+    );
+
+    public int solution(String dirs) {
+        int answer = 0;
+        Map<Point, Set<Point>> routes = new HashMap<>();
+        Point cur = new Point(0, 0);
+        routes.put(cur, new HashSet<>());
+
+        for (String d : dirs.split("")) {
+            int[] move = moves.get(d);
+            Point next = new Point(cur.getX() + move[0], cur.getY() + move[1]);
+
+            if (!next.inBound()) {
+                continue;
+            }
+
+            if (!routes.containsKey(next)) {
+                routes.put(next, new HashSet<>());
+            }
+
+            routes.get(cur).add(next);
+            routes.get(next).add(cur);
+            cur = next;
+        }
+
+        for (Point p : routes.keySet()) {
+            answer += routes.get(p).size();
+        }
+
+        return answer / 2;
+    }
+
+    private static class Point {
+        private final int x;
+        private final int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public boolean inBound() {
+            return x >= -5 && x <= 5 && y >= -5 && y <= 5;
+        }
+
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+
+            Point p = (Point) obj;
+            return x == p.getX() && y == p.getY();
+        }
+
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+}

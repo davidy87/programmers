338729442da2c37e4165b0a6b4a00eba473dd812
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2024-05-04 12:22:08 +0900

    문제 풀이 - 주차 요금 계산(Lv. 2)

---
+++ src/kakao/blind/_2022/주차_요금_계산.java
@@ -0,0 +1,54 @@
+package kakao.blind._2022;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class 주차_요금_계산 {
+
+    private static final String MAX_TIME = "23:59";
+
+    public int[] solution(int[] fees, String[] records) {
+        Map<String, Integer> cost = new TreeMap<>();
+        Map<String, Integer> parking = new HashMap<>();
+
+        for (String r : records) {
+            String[] record = r.split(" ");
+            String time = record[0];
+            String number = record[1];
+            String status = record[2];
+
+            if (status.equals("IN")) {
+                parking.put(number, toMinutes(time));
+            } else {
+                int duration = toMinutes(time) - parking.get(number);
+                cost.put(number, cost.getOrDefault(number, 0) + duration);
+                parking.remove(number);
+            }
+        }
+
+        for (String number : parking.keySet()) {
+            int duration = toMinutes(MAX_TIME) - parking.get(number);
+            cost.put(number, cost.getOrDefault(number, 0) + duration);
+        }
+
+        cost.replaceAll((number, duration) -> calculateFee(fees, duration));
+
+        return cost.values().stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private int calculateFee(int[] fees, int duration) {
+        int totalFee = fees[1];
+
+        if (duration > fees[0]) {
+            totalFee += ((int) Math.ceil(((double) (duration - fees[0])) / fees[2])) * fees[3];
+        }
+
+        return totalFee;
+    }
+
+    private int toMinutes(String time) {
+        String[] split = time.split(":");
+        return Integer.parseInt(split[0]) * 60 + Integer.parseInt(split[1]);
+    }
+}

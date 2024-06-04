00013a305338ba2c3b0825b67b546e675199e5bc
Jungwoong (David) Yoon <dyoon0807@gmail.com>
2024-06-04 15:56:45 +0900

    문제 풀이
    - 배달(Lv. 2)
    
    - 방금그곡(Lv. 2)

---
+++ src/lv2/방금그곡.java
@@ -0,0 +1,51 @@
+package lv2;
+
+public class 방금그곡 {
+
+    public String solution(String m, String[] musicinfos) {
+        String answer = "(None)";
+        int maxPlayTime = 0;
+        m = convertChords(m);
+
+        for (String info : musicinfos) {
+            String[] split = info.split(",");
+            int startTime = convertTimeToMin(split[0]);
+            int endTime = convertTimeToMin(split[1]);
+            int playTime = endTime - startTime;
+            String title = split[2];
+            String chords = getAllChords(convertChords(split[3]), playTime);
+
+            if (chords.contains(m) && maxPlayTime < playTime) {
+                answer = title;
+                maxPlayTime = playTime;
+            }
+        }
+
+        return answer;
+    }
+
+    private String getAllChords(String chords, int playTime) {
+        int numPlay = playTime / chords.length();
+        int rest = playTime % chords.length();
+
+        return chords.repeat(numPlay) + chords.substring(0, rest);
+    }
+
+    private String convertChords(String chords) {
+        String[] high = {"C", "D", "F", "G", "A", "B"};
+
+        for (String h : high) {
+            chords = chords.replace(h + "#", h.toLowerCase());
+        }
+
+        return chords;
+    }
+
+    private int convertTimeToMin(String time) {
+        String[] split = time.split(":");
+        int hours = Integer.parseInt(split[0]);
+        int mins = Integer.parseInt(split[1]);
+
+        return hours * 60 + mins;
+    }
+}
---
+++ src/summer_winter_coding/배달.java
@@ -0,0 +1,69 @@
+package summer_winter_coding;
+
+import java.util.*;
+
+public class 배달 {
+
+    public int solution(int N, int[][] road, int K) {
+        Map<Integer, Set<int[]>> map = createMap(road);
+        return bfs(map, N, K);
+    }
+
+    private int bfs(Map<Integer, Set<int[]>> map, int N, int K) {
+        int count = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        int[] visited = new int[N + 1];
+        queue.offer(new int[] {1, 0});
+
+        for (int i = 2; i <= N; i++) {
+            visited[i] = -1;
+        }
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int u = cur[0];
+            int dist = cur[1];
+
+            for (int[] next : map.get(u)) {
+                int v = next[0];
+                int newDist = dist + next[1];
+
+                if (visited[v] == -1 || newDist < visited[v]) {
+                    visited[v] = newDist;
+                    queue.offer(new int[] {v, newDist});
+                }
+            }
+        }
+
+        for (int i = 1; i < visited.length; i++) {
+            if (visited[i] <= K) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private Map<Integer, Set<int[]>> createMap(int[][] road) {
+        Map<Integer, Set<int[]>> map = new HashMap<>();
+
+        for (int[] r : road) {
+            int a = r[0];
+            int b = r[1];
+            int c = r[2];
+
+            if (!map.containsKey(a)) {
+                map.put(a, new HashSet<>());
+            }
+
+            if (!map.containsKey(b)) {
+                map.put(b, new HashSet<>());
+            }
+
+            map.get(a).add(new int[] {b, c});
+            map.get(b).add(new int[] {a, c});
+        }
+
+        return map;
+    }
+}

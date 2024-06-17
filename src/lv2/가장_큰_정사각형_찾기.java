988c1f856da9a0c10201a0e110217bc271a5642f
david_yoon_87 <jy8963@utexas.edu>
2024-06-17 12:03:19 +0900

    문제 풀이
    - 리코쳇 로봇 (Lv. 2)
    
    - 가장 큰 정사각형 찾기 (Lv. 2)

--- src/lv2/가장_큰_정사각형_찾기.java
+++ src/lv2/가장_큰_정사각형_찾기.java
@@ -3,22 +3,20 @@ package lv2;
 public class 가장_큰_정사각형_찾기 {
 
     public int solution(int[][] board) {
-        int answer = 0;
-
-        if (board.length < 2 && board[0].length < 2) {
-            return board[0][0];
-        }
+        int answer = (int) Math.pow(board[0][0], 2);
 
         for (int r = 1; r < board.length; r++) {
-            for (int c = 1; c < board[0].length; c++) {
+            for (int c = 1; c < board[r].length; c++) {
                 if (board[r][c] == 1) {
-                    board[r][c] = Math.min(board[r - 1][c], Math.min(board[r][c - 1], board[r - 1][c - 1])) + 1;
+                    int n1 = board[r - 1][c];
+                    int n2 = board[r][c - 1];
+                    int n3 = board[r - 1][c - 1];
+                    board[r][c] = Math.min(n1, Math.min(n2, n3)) + 1;
+                    answer = Math.max((int) Math.pow(board[r][c], 2), answer);
                 }
-
-                answer = Math.max(answer, board[r][c]);
             }
         }
 
-        return (int) Math.pow(answer, 2);
+        return answer;
     }
 }
--- src/lv2/리코쳇_로봇.java
+++ src/lv2/리코쳇_로봇.java
@@ -3,60 +3,66 @@ package lv2;
 import java.util.*;
 
 public class 리코쳇_로봇 {
+    private static final char START = 'R';
+    private static final char OBSTACLE = 'D';
+    private static final char GOAL = 'G';
 
     private static final int[] dr = {0, 1, 0, -1};
     private static final int[] dc = {1, 0, -1, 0};
 
     public int solution(String[] board) {
-        int answer = 0;
-        int[] start = new int[2];
-        boolean startFound = false;
-
-        for (int r = 0; r < board.length && !startFound; r++) {
-            for (int c = 0; c < board[0].length() && !startFound; c++) {
-                if (board[r].charAt(c) == 'R') {
-                    start = new int[] {r, c};
-                    startFound = true;
-                }
-            }
-        }
-
-        return search(board, start);
+        int[] startPos = findStartPos(board);
+        return bfs(board, startPos);
     }
 
-    private int search(String[] board, int[] start) {
+    private int bfs(String[] board, int[] start) {
         Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[board.length][board[0].length()];
         queue.offer(new int[] {start[0], start[1], 0});
+        int[][] record = new int[board.length][board[0].length()];
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
-            visited[cur[0]][cur[1]] = true;
+            record[cur[0]][cur[1]] = cur[2];
 
-            if (board[cur[0]].charAt(cur[1]) == 'G') {
+            if (board[cur[0]].charAt(cur[1]) == GOAL) {
                 return cur[2];
             }
 
             for (int i = 0; i < 4; i++) {
-                int newR = cur[0] + dr[i];
-                int newC = cur[1] + dc[i];
-                boolean inBound = newR >= 0 && newR < board.length && newC >= 0 && newC < board[0].length();
+                int newR = cur[0];
+                int newC = cur[1];
+                int newCount = cur[2] + 1;
 
-                while (inBound && board[newR].charAt(newC) != 'D') {
+                while (inBound(board, newR, newC) && board[newR].charAt(newC) != OBSTACLE) {
                     newR += dr[i];
                     newC += dc[i];
-                    inBound = newR >= 0 && newR < board.length && newC >= 0 && newC < board[0].length();
                 }
 
                 newR -= dr[i];
                 newC -= dc[i];
 
-                if (!visited[newR][newC]) {
-                    queue.offer(new int[] {newR, newC, cur[2] + 1});
+                if (record[newR][newC] == 0 || newCount < record[newR][newC]) {
+                    queue.offer(new int[] {newR, newC, newCount});
                 }
             }
         }
 
         return -1;
+    }
+
+    private boolean inBound(String[] board, int r, int c) {
+        return r >= 0 && r < board.length && c >= 0 && c < board[r].length();
+    }
+
+    private int[] findStartPos(String[] board) {
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[r].length(); c++) {
+                if (board[r].charAt(c) == START) {
+                    return new int[] {r, c};
+                }
+            }
+        }
+
+        return new int[2];
     }
 }

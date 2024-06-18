5b03392cb96d1ea734ce6a24ce72ca56f8f49eee
david_yoon_87 <dyoon0807@gmail.com>
2024-06-18 16:32:02 +0900

    문제 풀이
    - 하노이의 탑 (Lv. 2)
    
    - 디펜스 게임 (Lv. 2)

--- src/lv2/디펜스_게임.java
+++ src/lv2/디펜스_게임.java
@@ -6,7 +6,7 @@ public class 디펜스_게임 {
 
     public int solution(int n, int k, int[] enemy) {
         int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
         for (int e : enemy) {
             n -= e;

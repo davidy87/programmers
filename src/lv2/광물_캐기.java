448f501a9394ca7c53ce178c3e7332acb47c3ed8
davidy87 <dyoon0807@gmail.com>
2024-06-21 12:46:10 +0900

    문제 풀이
    - 광물 캐기 (Lv. 2)
    
    - 우박수열 정적분 (Lv. 2)

--- src/lv2/광물_캐기.java
+++ src/lv2/광물_캐기.java
@@ -4,13 +4,30 @@ import java.util.*;
 
 public class 광물_캐기 {
 
-    private static final int[][] fatigue = {{1, 1, 1}, {5, 1, 1}, {25, 5, 1}};
+    private static final int[][] FATIGUES = {{1, 1, 1}, {5, 1, 1}, {25, 5, 1}};
+
     private static final Map<String, Integer> idxMap = Map.of("diamond", 0, "iron", 1, "stone", 2);
 
     public int solution(int[] picks, String[] minerals) {
         int answer = 0;
+        Queue<int[]> records = getRecords(picks, minerals);
+
+        while (!records.isEmpty()) {
+            for (int p = 0; p < 3; p++) {
+                if (picks[p] > 0) {
+                    answer += records.poll()[p];
+                    picks[p]--;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private Queue<int[]> getRecords(int[] picks, String[] minerals) {
         int totalPicks = picks[0] + picks[1] + picks[2];
-        List<int[]> recordList = new ArrayList<>();
+        Queue<int[]> records = new PriorityQueue<>((r1, r2) -> r2[2] - r1[2]);
 
         for (int i = 0; i < minerals.length; i += 5) {
             if (totalPicks == 0) {
@@ -19,30 +36,16 @@ public class 광물_캐기 {
 
             int[] record = new int[3];
 
-            for (int j = i; j < i + 5 && j < minerals.length; j++) {
-                String m = minerals[j];
-
+            for (int m = i; m < i + 5 && m < minerals.length; m++) {
                 for (int p = 0; p < 3; p++) {
-                    record[p] += fatigue[p][idxMap.get(m)];
+                    record[p] += FATIGUES[p][idxMap.get(minerals[m])];
                 }
             }
 
-            recordList.add(record);
+            records.offer(record);
             totalPicks--;
         }
 
-        recordList.sort((r1, r2) -> r2[2] - r1[2]);
-
-        for (int[] record : recordList) {
-            for (int p = 0; p < 3; p++) {
-                if (picks[p] > 0) {
-                    answer += record[p];
-                    picks[p]--;
-                    break;
-                }
-            }
-        }
-
-        return answer;
+        return records;
     }
 }
--- src/lv2/우박수열_정적분.java
+++ src/lv2/우박수열_정적분.java
@@ -6,26 +6,20 @@ public class 우박수열_정적분 {
 
     public double[] solution(int k, int[][] ranges) {
         double[] answer = new double[ranges.length];
-        List<Integer> collatzList = new ArrayList<>();
-        collatz(collatzList, k);
-        collatzList.add(1);
+        List<Integer> sequence = new ArrayList<>();
+        collatz(k, sequence);
 
-        double[] area = new double[collatzList.size() - 1];
-
-        for (int i = 0; i < area.length; i++) {
-            area[i] = (collatzList.get(i) + collatzList.get(i + 1)) / 2.0;
-        }
+        double[] area = getArea(sequence);
 
         for (int i = 0; i < ranges.length; i++) {
-            int start = ranges[i][0];
-            int end = area.length + ranges[i][1];
+            int x1 = ranges[i][0];
+            int x2 = sequence.size() - 1 + ranges[i][1];
 
-            if (start > end) {
+            if (x1 > x2) {
                 answer[i] = -1.0;
-                continue;
             }
 
-            for (int j = start; j < end; j++) {
+            for (int j = x1; j < x2; j++) {
                 answer[i] += area[j];
             }
         }
@@ -33,12 +27,25 @@ public class 우박수열_정적분 {
         return answer;
     }
 
-    private void collatz(List<Integer> list, int k) {
-        list.add(k);
-        k = k % 2 == 0 ? k / 2 : k * 3 + 1;
+    private double[] getArea(List<Integer> sequence) {
+        double[] area = new double[sequence.size()];
 
-        if (k > 1) {
-            collatz(list, k);
+        for (int i = 0; i < sequence.size() - 1; i++) {
+            area[i] = (sequence.get(i) + sequence.get(i + 1)) / 2.0;
+        }
+
+        return area;
+    }
+
+    private void collatz(int k, List<Integer> sequence) {
+        sequence.add(k);
+
+        if (k == 1) {
+            return;
+        } else if (k % 2 == 0) {
+            collatz(k / 2, sequence);
+        } else {
+            collatz(k * 3 + 1, sequence);
         }
     }
 }

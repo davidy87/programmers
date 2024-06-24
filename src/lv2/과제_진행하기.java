424306e3428169f22845873fe0cd828027d65d74
davidy87 <dyoon0807@gmail.com>
2024-06-24 14:15:25 +0900

    문제 풀이
    - 점 찍기 (Lv. 2)
    
    - 과제 진행하기 (Lv. 2)

--- src/lv2/과제_진행하기.java
+++ src/lv2/과제_진행하기.java
@@ -3,61 +3,65 @@ package lv2;
 import java.util.*;
 
 public class 과제_진행하기 {
+    private final List<String> answer = new ArrayList<>();
+    private final Queue<String[]> schedule = new PriorityQueue<>((p1, p2) -> convertTime(p1[1]) - convertTime(p2[1]));
+    private final Deque<String[]> pending = new ArrayDeque<>();
 
     public String[] solution(String[][] plans) {
-        String[] answer = new String[plans.length];
-        Stack<String[]> pending = new Stack<>();
-        PriorityQueue<String[]> pq = new PriorityQueue<>((p1, p2) -> convertTime(p1[1]) - convertTime(p2[1]));
+        makeSchedule(plans);
+        startProjects();
 
-        for (String[] plan : plans) {
-            pq.offer(plan);
-        }
+        return answer.toArray(new String[plans.length]);
+    }
 
-        int answerIdx = 0;
+    private void startProjects() {
+        while (schedule.size() >= 2) {
+            String[] curPlan = schedule.poll();
+            String name = curPlan[0];
+            int start = convertTime(curPlan[1]);
+            int playtime = Integer.parseInt(curPlan[2]);
+            int nextStart = convertTime(schedule.peek()[1]);
+            int timeGap = nextStart - (start + playtime);
 
-        while (pq.size() >= 2) {
-            String[] cur = pq.poll();
-            String name = cur[0];
-            int start = convertTime(cur[1]);
-            int playtime = Integer.parseInt(cur[2]);
-            int nextStart = convertTime(pq.peek()[1]);
-            int timeLeft = nextStart - (start + playtime);
-
-            if (timeLeft < 0) {
-                pending.push(new String[] {name, String.valueOf(playtime - (nextStart - start))});
+            if (timeGap < 0) {
+                pending.offer(new String[] {name, String.valueOf(playtime - (nextStart - start))});
                 continue;
             }
 
-            answer[answerIdx++] = name;
+            answer.add(name);
 
-            while (!pending.isEmpty() && timeLeft > 0) {
-                int pendingTime = Integer.parseInt(pending.peek()[1]);
+            while (!pending.isEmpty() && timeGap > 0) {
+                String[] p = pending.pollLast();
+                int leftover = Integer.parseInt(p[1]);
 
-                if (timeLeft >= pendingTime) {
-                    timeLeft -= pendingTime;
-                    answer[answerIdx++] = pending.pop()[0];
+                if (timeGap >= leftover) {
+                    timeGap -= leftover;
+                    answer.add(p[0]);
                 } else {
-                    String[] p = pending.pop();
-                    p[1] = String.valueOf(Integer.parseInt(p[1]) - timeLeft);
-                    pending.push(p);
+                    p[1] = String.valueOf(leftover - timeGap);
+                    pending.offer(p);
                     break;
                 }
             }
         }
 
-        answer[answerIdx++] = pq.poll()[0];
+        answer.add(schedule.poll()[0]);
 
         while (!pending.isEmpty()) {
-            answer[answerIdx++] = pending.pop()[0];
+            answer.add(pending.pollLast()[0]);
         }
-
-        return answer;
     }
 
-    private int convertTime(String t) {
-        String[] splited = t.split(":");
-        int hours = Integer.parseInt(splited[0]);
-        int mins = Integer.parseInt(splited[1]);
+    private void makeSchedule(String[][] plans) {
+        for (String[] plan : plans) {
+            schedule.offer(plan);
+        }
+    }
+
+    private int convertTime(String time) {
+        String[] split = time.split(":");
+        int hours = Integer.parseInt(split[0]);
+        int mins = Integer.parseInt(split[1]);
 
         return hours * 60 + mins;
     }
--- src/lv2/점_찍기.java
+++ src/lv2/점_찍기.java
@@ -4,10 +4,11 @@ public class 점_찍기 {
 
     public long solution(int k, int d) {
         long answer = 0;
+        double dSquared = Math.pow(d, 2);
 
         for (int x = 0; x <= d; x += k) {
-            double y =  Math.sqrt(Math.pow(d, 2) - Math.pow(x, 2));
-            answer += (long) (Math.ceil(y / k) + (y % k == 0 ? 1 : 0));
+            int y = (int) Math.sqrt(dSquared - Math.pow(x, 2));
+            answer += y / k + 1;
         }
 
         return answer;

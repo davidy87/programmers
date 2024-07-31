b8308df31cad2af9d6b54e5753d83ab3b0a0d308
davidy87 <dyoon0807@gmail.com>
2024-07-31 11:47:05 +0900

    문제 풀이
    - 최고의 집합 (Lv. 3)
    
    - 숫자 게임 (Lv. 3)

--- src/lv3/숫자_게임.java
+++ src/lv3/숫자_게임.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 public class 숫자_게임 {
 
     public int solution(int[] A, int[] B) {
-        Arrays.sort(A);
-        Arrays.sort(B);
-
         int answer = 0;
         int idxA = 0;
         int idxB = 0;
 
-        while (idxA < A.length && idxB < B.length) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        while (idxB < B.length) {
             if (A[idxA] < B[idxB]) {
                 answer++;
                 idxA++;
--- src/lv3/최고의_집합.java
+++ src/lv3/최고의_집합.java
@@ -5,25 +5,19 @@ import java.util.*;
 public class 최고의_집합 {
 
     public int[] solution(int n, int s) {
-        if (n > s) {
+        if (n >= s) {
             return new int[] {-1};
         }
 
         int[] answer = new int[n];
-        int remainder = s % n;
-        int idx = 0;
+        int leftOver = s % n;
+        int i = n - 1;
         Arrays.fill(answer, s / n);
 
-        while (remainder > 0) {
-            if (idx == n) {
-                idx = 0;
-            }
-
-            answer[idx++]++;
-            remainder--;
+        while (leftOver > 0) {
+            answer[i--]++;
+            leftOver--;
         }
-
-        Arrays.sort(answer);
 
         return answer;
     }

716d2646b7757af08066491161a67244432096b2
davidy87 <dyoon0807@gmail.com>
2024-08-21 12:52:13 +0900

    문제 풀이
    - 연속 펄스 부분 수열의 합 (Lv. 3)
    
    - 가장 긴 팰린드롬 (Lv. 3)

--- src/lv3/가장_긴_팰린드롬.java
+++ src/lv3/가장_긴_팰린드롬.java
@@ -3,19 +3,19 @@ package lv3;
 public class 가장_긴_팰린드롬 {
 
     public int solution(String s) {
-        for (int i = s.length(); i >= 1; i--) {
-            for (int j = 0; j + i <= s.length(); j++) {
-                if (isPalindrome(s, j, j + i - 1)) {
-                    return i;
+        for (int range = s.length(); range >= 1; range--) {
+            for (int start = 0; start + range <= s.length(); start++) {
+                if (isPalindrome(s, start, start + range - 1)) {
+                    return range;
                 }
             }
         }
 
-        return 0;
+        return 1;
     }
 
     private boolean isPalindrome(String s, int start, int end) {
-        while (start <= end) {
+        while (start < end) {
             if (s.charAt(start++) != s.charAt(end--)) {
                 return false;
             }
--- src/lv3/연속_펄스_부분_수열의_합.java
+++ src/lv3/연속_펄스_부분_수열의_합.java
@@ -3,18 +3,17 @@ package lv3;
 public class 연속_펄스_부분_수열의_합 {
 
     public long solution(int[] sequence) {
-        long answer = 0;
         long[][] dp = new long[sequence.length][2];
-
         dp[0][0] = sequence[0];
         dp[0][1] = -sequence[0];
-        answer = Math.max(dp[0][0], dp[0][1]);
+
+        long answer = Math.max(dp[0][0], dp[0][1]);
 
         for (int i = 1; i < sequence.length; i++) {
-            long s = sequence[i];
-            dp[i][0] = Math.max(s, dp[i - 1][1] + s);
-            dp[i][1] = Math.max(-s, dp[i - 1][0] - s);
-            answer = Math.max(answer, Math.max(dp[i][0], dp[i][1]));
+            int num = sequence[i];
+            dp[i][0] = Math.max(num, dp[i - 1][1] + num);
+            dp[i][1] = Math.max(-num, dp[i - 1][0] - num);
+            answer = Math.max(Math.max(dp[i][0], dp[i][1]), answer);
         }
 
         return answer;

7ad938fc4d0f10e2c6507ccc3b55fd7e1e9ec94c
davidy87 <dyoon0807@gmail.com>
2024-09-02 13:56:44 +0900

    문제 풀이
    - 합승 택시 요금 (Lv. 3)

---
+++ src/lv3/합승_택시_요금.java
@@ -0,0 +1,72 @@
+package lv3;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/*
+    Dijkstra Algorithm
+ */
+public class 합승_택시_요금 {
+
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        int answer = Integer.MAX_VALUE;
+        int[][] map = createMap(fares, n);
+        int[] costsFromS = dijkstra(map, n, s);
+        int[] costsFromA = dijkstra(map, n, a);
+        int[] costsFromB = dijkstra(map, n, b);
+
+        for (int i = 1; i <= n; i++) {
+            int cost = costsFromS[i] + costsFromA[i] + costsFromB[i];
+            answer = Math.min(cost, answer);
+        }
+
+        return answer;
+    }
+
+    private int[] dijkstra(int[][] map, int n, int s) {
+        Queue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(node -> node[1]));
+        int[] costs = new int[n + 1];
+        boolean[] visited = new boolean[n + 1];
+
+        Arrays.fill(costs, Integer.MAX_VALUE);
+        queue.offer(new int[] {s, 0});
+        costs[s] = 0;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int u = cur[0];
+
+            if (visited[u]) {
+                continue;
+            }
+
+            for (int v = 1; v <= n; v++) {
+                if (map[u][v] == 0) {
+                    continue;
+                }
+
+                int newCost = costs[u] + map[u][v];
+
+                if (newCost < costs[v]) {
+                    costs[v] = newCost;
+                    queue.offer(new int[] {v, costs[v]});
+                }
+            }
+        }
+
+        return costs;
+    }
+
+    private int[][] createMap(int[][] fares, int n) {
+        int[][] map = new int[n + 1][n + 1];
+
+        for (int[] f : fares) {
+            map[f[0]][f[1]] = f[2];
+            map[f[1]][f[0]] = f[2];
+        }
+
+        return map;
+    }
+}

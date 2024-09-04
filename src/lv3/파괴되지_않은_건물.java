0e87f9cd0be367793b8f42b4fe7bed589d2ade68
davidy87 <dyoon0807@gmail.com>
2024-09-04 12:16:54 +0900

    문제 풀이
    - 파괴되지 않은 건물 (Lv. 3)

--- src/lv3/파괴되지_않은_건물.java
+++ src/lv3/파괴되지_않은_건물.java
@@ -2,56 +2,57 @@ package lv3;
 
 public class 파괴되지_않은_건물 {
 
-    private int N, M;
-
     public int solution(int[][] board, int[][] skill) {
-        N = board.length;
-        M = board[0].length;
-        int[][] damageSum = new int[N + 1][M + 1];
+        int[][] damage = new int[board.length + 1][board[0].length + 1];
 
         for (int[] s : skill) {
-            int r1 = s[1];
-            int c1 = s[2];
-            int r2 = s[3];
-            int c2 = s[4];
-            int degree = s[0] == 1 ? -s[5] : s[5];
-
-            damageSum[r1][c1] += degree;
-            damageSum[r1][c2 + 1] -= degree;
-            damageSum[r2 + 1][c1] -= degree;
-            damageSum[r2 + 1][c2 + 1] += degree;
+            cumulateDamage(damage, s);
         }
 
-        action(damageSum);
+        calculateDamage(damage);
 
-        return countSurvived(board, damageSum);
+        return countSurvived(board, damage);
     }
 
-    private int countSurvived(int[][] board, int[][] damageSum) {
-        int count = 0;
+    private int countSurvived(int[][] board, int[][] damage) {
+        int result = 0;
 
         for (int r = 0; r < board.length; r++) {
             for (int c = 0; c < board[0].length; c++) {
-                if (board[r][c] + damageSum[r][c] > 0) {
-                    count++;
+                if (board[r][c] + damage[r][c] > 0) {
+                    result++;
                 }
             }
         }
 
-        return count;
+        return result;
     }
 
-    private void action(int[][] damageSum) {
-        for (int r = 0; r <= N; r++) {
-            for (int c = 1; c <= M; c++) {
-                damageSum[r][c] += damageSum[r][c - 1];
+    private void calculateDamage(int[][] damage) {
+        for (int c = 0; c < damage[0].length; c++) {
+            for (int r = 1; r < damage.length; r++) {
+                damage[r][c] += damage[r - 1][c];
             }
         }
 
-        for (int c = 0; c <= M; c++) {
-            for (int r = 1; r <= N; r++) {
-                damageSum[r][c] += damageSum[r - 1][c];
+        for (int r = 0; r < damage.length; r++) {
+            for (int c = 1; c < damage[0].length; c++) {
+                damage[r][c] += damage[r][c - 1];
             }
         }
+    }
+
+    private void cumulateDamage(int[][] damage, int[] s) {
+        int type = s[0];
+        int r1 = s[1];
+        int c1 = s[2];
+        int r2 = s[3];
+        int c2 = s[4];
+        int degree = type == 1 ? -s[5] : s[5];
+
+        damage[r1][c1] += degree;
+        damage[r1][c2 + 1] -= degree;
+        damage[r2 + 1][c1] -= degree;
+        damage[r2 + 1][c2 + 1] += degree;
     }
 }
